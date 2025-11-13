177fb850c3664a077fe831ac4b4c99e3a2b7700f
Mike Adeleke <oikeoluwa@gmail.com>
2025-11-13 21:13:24 +0100

    fix mobile responsiveness

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,31 @@
+// // components/Layout.js
+// import Header from './Header';
+// import Footer from './Footer';
+// import ScrollToTopButton from './ScrollToTopButton';
+
+// const Layout = ({ children }) => {
+//   return (
+//     <div>
+//       <Header />
+//       <main>{children}</main>
+//       <ScrollToTopButton />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
 // components/Layout.js
-import Header from './Header';
-import Footer from './Footer';
-import ScrollToTopButton from './ScrollToTopButton';
+import Header from "./Header";
+import Footer from "./Footer";
+import ScrollToTopButton from "./ScrollToTopButton";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="overflow-x-hidden w-full max-w-full">
       <Header />
-      <main>{children}</main>
+      <main className="overflow-x-hidden w-full max-w-full">{children}</main>
       <ScrollToTopButton />
       <Footer />
     </div>
